068e6bf84436face399cf9ec7b2b96648c8b6637
FreedomSakura <66147729+FreedomSakura@users.noreply.github.com>
2025-01-06 23:19:02 +0800

    在飞连的围剿下修复版本到LRU

---
+++ Packages/cn.etetet.login/Scripts/Model/Server/CoroutineLockType.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ET
+{
+    public static partial class CoroutineLockType
+    {
+        public const int LoginAccount = PackageType.Login * 1000 + 1;
+
+        public const int LoginCenterLock = PackageType.Login * 1000 + 2;
+
+        public const int LoginGate = PackageType.Login * 1000 + 3;
+
+        public const int CreateRole = PackageType.Login * 1000 + 4;
+    }
+}
